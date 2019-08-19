238df08bae19cacf0b2c79b0ad530c7fb26fe15c
Anurag Mathur <anurag.mathur@cmdsolutions.com.au>
2019-08-19 22:16:38 +1000

    Updated R53 Hosted zones and records; Updated Readme ; Added deploy.sh

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # awslamp
 
-cfn-flip "dev-params.yml" "dev-params.json"
-aws cloudformation create-stack --stack-name "lamp-stack" --template-body file://lamp.yml --parameters file://dev-params.json --region=ap-southeast-2 --capabilities CAPABILITY_IAM
+## The repo is to create a lamp stack
+## Resources Deployed :
+
+1. VPC
+2. Subnets ( Public and Private )
+3. Route Tables
+4. NACLs
+5. Security Groups
+6. Internet Gateway
+7. IAM Instance Profile
+8. ASG
+9. RDS
+10. ALB ( including Target group and Listener )
+11. Internal R53 Hosted Zone
+12. R53 record for the ALB and RDS
+
+How To ?
+-> Run the script deploy.sh with an argument which is the env name
+  Example : ./deploy.sh dev  OR ./deploy.sh prod
+There is a sample parameter file for prod and dev, which has parameters that can be changed.
+deploy.sh changes the params yml file to a json file
+The json file is then used with the create-stack and update-stack command for parameter inputs.
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 env=$1
-cfn-flip "${env}-params.yml" "${env}-params.json"
-aws cloudformation create-stack --stack-name "lamp-${env}" --template-body file://lamp.yml --region=ap-southeast-2 --capabilities CAPABILITY_IAM --parameters file://"${env}"-params.json --profile=personal
+
+echo "Deploying ${env} environment "
+params_file="${env}"-params.yml
+if test -f "$params_file"; then
+  echo "$params_file exist.. Continue"
+  cfn-flip "${env}-params.yml" "${env}-params.json"
+else
+  echo "Parameters file for ${env} does not exist. Please create one and run again."
+  exit 1
+fi
+
+stackname=$(aws cloudformation describe-stacks --region=ap-southeast-2 --profile=personal | jq '.Stacks[].StackName' | grep "lamp-${env}" )
+
+if [ $? == 0 ]; then
+  echo "stack exists.. need to be updated"
+  echo "Updating $stackname ... "
+  aws cloudformation update-stack --stack-name "lamp-${env}" --template-body file://lamp.yml --region=ap-southeast-2 --capabilities CAPABILITY_IAM --parameters file://"${env}"-params.json --profile=personal
+  if [ $? == 0 ]; then
+    echo "stack Update in progress. Please check the cloudformation console/cli for events and resources"
+  else
+    echo "Stack could not be updated"
+  fi  
+else
+  echo "Stack DOES NOT exist. Creating"
+  echo "Creating $stackname ... "
+  aws cloudformation create-stack --stack-name "lamp-${env}" --template-body file://lamp.yml --region=ap-southeast-2 --capabilities CAPABILITY_IAM --parameters file://"${env}"-params.json --profile=personal
+  if [ $? == 0 ]; then
+    echo "stack creation in progress. Please check the cloudformation console/cli for events and resources"
+  else
+    echo "Stack could not be created"
+  fi
+fi
